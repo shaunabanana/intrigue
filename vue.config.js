e710e0d3dac11b5127c741367d61cffe549b0906
Shengchen Zhang <shengchenzhang1207@gmail.com>
2022-08-14 03:38:46 +0800

    Web version polyfills

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = defineConfig({
     transpileDependencies: true,
     configureWebpack: {
+        plugins: [new NodePolyfillPlugin()],
+        resolve: {
+            fallback: {
+                fs: false,
+                'fs/promises': false,
+            },
+        },
         module: {
             rules: [
                 {
