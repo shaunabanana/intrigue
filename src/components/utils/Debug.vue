d043f96554dfbda989a2fc06f00ba5ce144ef731
Shengchen Zhang <shengchenzhang1207@gmail.com>
2022-08-16 11:44:19 +0800

    Add import function to debug panel

--- src/components/utils/Debug.vue
+++ src/components/utils/Debug.vue
@@ -5,8 +5,20 @@
         <pre>{{currentContext}}</pre>
 
         <a-space>
+            <a-button @click="showImportModal = true">Import Data</a-button>
             <a-button @click="exportJSON">Export Data</a-button>
         </a-space>
+
+        <a-modal v-model:visible="showImportModal"
+            @ok="importJSON" @cancel="jsonData = ''"
+        >
+            <template #title>
+                Import JSON data
+            </template>
+            <div>
+                <a-textarea v-model="jsonData" placeholder="Paste JSON data here" allow-clear/>
+            </div>
+    </a-modal>
     </a-card>
 </template>
 
@@ -15,11 +27,28 @@ import { stringify } from 'yaml';
 
 export default {
     name: 'IntrigueDebug',
-    inject: ['state', 'store'],
+    inject: ['document', 'state', 'store'],
+
+    data() {
+        return {
+            showImportModal: false,
+            jsonData: '',
+        };
+    },
 
     methods: {
         exportJSON() {
             console.log(JSON.stringify(this.store.value));
+        },
+
+        importJSON() {
+            console.log(this.jsonData);
+            console.log(JSON.parse(this.jsonData));
+            this.document.store = JSON.parse(this.jsonData);
+            this.document.unbindSyncHandler();
+            this.document.updateSyncedData();
+            this.document.unbindCommitHandler = this.document.on('commit', this.document.updateSyncedData.bind(this.document));
+            this.showImportModal = false;
         },
     },
 
